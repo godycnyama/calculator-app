551c5736b3eb90942367a95823a7845f3527e8d1
Godknows Christians Nyamatendedza <godycnyama@gmail.com>
2023-12-20 23:44:53 +0200

    removed used component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,50 +5,88 @@ import BackspaceIcon from '@mui/icons-material/Backspace';
 
 
 const Calculator = () => {
-  const [display, setDisplay] = useState('0');
   const [cdisplay, setCdisplay] = useState('0');
   const [history, setHistory] = useState([]);
+  const [runningTotal, setRunningTotal] = useState(0);
+  const [calculations, setCalculations] = useState([]);
   
   const deleteLast = () => {
-
+    let dis = cdisplay.toString();
+    if(dis.length === 1) {
+      setCdisplay('0');
+    } else {
+      setCdisplay(dis.slice(0, -1));
+    }
   }
 
   const clear = () => {
-
+    if(calculations.length >= 2) {
+      let temp = [...calculations];
+      temp.shift();
+      temp.push(cdisplay);
+      setCalculations(temp);
+      console.log(calculations);
+    } else {
+      setCalculations([...calculations, cdisplay]);
+    }
+    setCdisplay('0');
   }
 
   const calculate = () => {
+    let str = cdisplay;
+    for(let i = 0; i < str.length; i++) {
+      if(str[i] === '+') {
+        let left = str.slice(0, i);
+        let right = str.slice(i + 1);
+        let result = Number(left) + Number(right);
+        setCdisplay(`${cdisplay}=${result}`);
+        break;
+      }
+      if(str[i] === '-') {
+        let left = str.slice(0, i);
+        let right = str.slice(i + 1);
+        let result = Number(left) - Number(right);
+        setCdisplay(`${cdisplay}=${result}`);
+        break;
+      }
+    }
 
   }
 
   const add = () => {
-
+    setCdisplay(cdisplay + '+');
   }
 
   const subtract = () => {
-   
+    setCdisplay(cdisplay + '-');
   }
 
   const equals = () => {
-   
+    calculate();
   }
 
   const showCalculationsHistory = () => {
     
   }
 
-  const handleNumberEvent = (e) => {
-    if (display === '0') {
-      setDisplay(e.target.id);
-    } else {
-      setDisplay(display + e.target.id);
-    }
+  const showTwoPreviousCalculations = () => {
+
   }
+
+  const handleNumberEvent = (data) => {
+    if(cdisplay === '0') {
+      setCdisplay(data);
+    } else {
+      setCdisplay(`${cdisplay}${data}`);
+    }
+  };
 
   return (
     <div className="frame">
     <div className="inner-frame">
-      <label id="display"> {display}</label>
+      <div id="display"> 
+        {calculations.map((calculation, index) => <div key={index}>{calculation}</div>)}  
+      </div>
       <label id="cdisplay" >{cdisplay}</label>
       <Button variant="contained" id="history" onClick={showCalculationsHistory}><HistoryIcon/></Button>
       <Button variant="contained" id="clear" onClick={clear}>AC</Button>
@@ -65,7 +103,7 @@ const Calculator = () => {
       <Button variant="contained" id="1" onClick={() => handleNumberEvent(1)}>1</Button>
       <Button variant="contained" id="2" onClick={() => handleNumberEvent(2)}>2</Button>
       <Button variant="contained" id="3" onClick={() => handleNumberEvent(3)}>3</Button>
-      <Button variant="contained" id="backspace"><BackspaceIcon/></Button>
+      <Button variant="contained" id="backspace" onClick={deleteLast}><BackspaceIcon/></Button>
       <Button variant="contained" id="zero">0</Button>
       <Button variant="contained" id="dot">.</Button>
       <Button variant="contained" id="equal" onClick={equals}>=</Button>
